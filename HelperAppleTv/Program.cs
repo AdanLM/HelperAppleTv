a3c62cd4abe243349769a2f0216ebc8fa6dbf273
AdanLM <adanmanzo94@gmail.com>
2020-12-16 12:39:00 -0600

    Add function to connect appleTv

--- HelperAppleTv/Program.cs
+++ HelperAppleTv/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Sockets;
+using Renci.SshNet;
+using Renci.SshNet.Common;
 
 namespace HelperAppleTv
 {
@@ -9,6 +9,30 @@ namespace HelperAppleTv
     {
         static void Main(string[] args)
         {
+            
+        }
+
+        private static void connectAppleTv(string IP, string user, string pass)
+        {
+            using (var client = new SshClient(IP, user, pass)) {
+                try
+                {
+                    client.Connect();
+                    client.RunCommand("reboot");
+                }
+                catch (SocketException e)
+                {
+                    //TODO - Notificacion IP no valida
+                }
+                catch (SshAuthenticationException e)
+                {
+                    //TODO - Notificacion usuario y/o password erroneas
+                }
+                finally
+                {
+                    client.Disconnect();
+                }
+            }
         }
     }
 }
